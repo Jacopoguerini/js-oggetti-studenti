f805675137757f076f1e073e83da56be212a750f
Jacopoguerini <jacopo.guerini1993@gmail.com>
2021-05-10 16:24:08 +0200

    Correzione ciclo for e inserimento prompt

--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ var student = {
 console.log(student);
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-for (var name in student) {
-    console.log(student.name, student.surname, student.age);
+for (var prop in student) {
+    console.log(student[prop]);
 }
 
 // Creare un array di oggetti di studenti.
@@ -47,7 +47,7 @@ var studentList = [
 
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 for (var i = 0; i < studentList.length; i++) {
-    console.log(studentList.name[i], studentList.surname[i]);
+    console.log(studentList[i].name, studentList[i].surname);
 }
 
 // Dare la possibilità all'utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell'ordine: nome, cognome e età.
@@ -57,7 +57,7 @@ var newStudent = {
     surname: prompt("Inserisci qui ili tuo cognome:"),
     age: prompt("Inserisci qui la tua età:")
 }
-
 console.log(newStudent);
+
 studentList.push(newStudent);
-console.log(studenList);
+console.log(studentList);
